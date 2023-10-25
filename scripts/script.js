df762e4bc453c0a0f9f3d990d73f3c445950b560
María Sol Arroyo <mariasolarroyoc.web@gmail.com>
2023-10-24 21:07:44 -0300

    Agregado de más animaciones y javascript

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+setInterval(function () {document.querySelector(".inicio-imagen").classList.toggle("movimiento")},7000);
+
+setInterval(function () {document.querySelector(".inicio-textos-h6").classList.toggle("movimiento")},2000);
