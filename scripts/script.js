7b90515d2d65ff95f167386b0eb72b347045afed
María Sol Arroyo <mariasolarroyoc.web@gmail.com>
2023-11-10 00:46:05 -0300

    ultimos detalles agregado de info real

--- scripts/script.js
+++ scripts/script.js
@@ -8,32 +8,32 @@ function infoCurso(n){
         let curso = document.getElementById("info-card-title1");
         let detalle = document.getElementById("info-detalle1");
         let precio = document.getElementById("info-precio1");
-alert("INFO:"+ "\n" + "- "  + curso.innerHTML + "\n" + "Detalle: " + "\n" + detalle.innerHTML + "\n" +"Precio: " + "\n" + precio.innerHTML + "\n");
+alert("INFO DEL CURSO:"+ "\n" + "- "  + curso.innerHTML  + "\n" + "- " + detalle.innerHTML + "\n"  +"- " + precio.innerHTML + "\n");
     }else  if (n == 2){
         let curso = document.getElementById("info-card-title2");
         let detalle = document.getElementById("info-detalle2");
         let precio = document.getElementById("info-precio2");
-    alert("CURSO: " + "\n" + curso.innerHTML + "\n" + "Detalle: " + "\n" + detalle.innerHTML + "\n" +"Precio: " + "\n" +precio.innerHTML + "\n");
+    alert("INFO DEL CURSO: " + "\n" +"- "+ curso.innerHTML  + "\n" + "- " + detalle.innerHTML  + "\n" +"- "  + precio.innerHTML + "\n");
     } else  if (n == 3){
         let curso = document.getElementById("info-card-title3");
         let detalle = document.getElementById("info-detalle3");
         let precio = document.getElementById("info-precio3");
-    alert("CURSO: " + "\n" + curso.innerHTML + "\n" + "Detalle: "+ "\n" +detalle.innerHTML + "\n" +"Precio: " + "\n" + precio.innerHTML + "\n");
+    alert("INFO DEL CURSO: " + "\n" + "- "+ curso.innerHTML + "\n"  + "- " +detalle.innerHTML + "\n" +"- " + precio.innerHTML + "\n");
     } else  if (n == 4){
         let curso = document.getElementById("info-card-title4");
         let detalle = document.getElementById("info-detalle4");
         let precio = document.getElementById("info-precio4");
-    alert("CURSO: " + "\n" + curso.innerHTML + "\n" + "Detalle: " + "\n" +detalle.innerHTML + "\n" +"Precio: " + "\n" + precio.innerHTML + "\n");
+    alert("INFO DEL CURSO: " + "\n" +"- "+ curso.innerHTML  + "\n" + "- " + detalle.innerHTML + "\n" +"- " + precio.innerHTML + "\n");
     } else  if (n == 5){
         let curso = document.getElementById("info-card-title5");
         let detalle = document.getElementById("info-detalle5");
         let precio = document.getElementById("info-precio5");
-    alert("CURSO: "+ "\n" + curso.innerHTML + "\n" + "Detalle: " + "\n" + detalle.innerHTML + "\n" +"Precio: " + "\n" +precio.innerHTML + "\n");
+    alert("INFO DEL CURSO: "+ "\n"+ "- " + curso.innerHTML  + "\n" + "- " + detalle.innerHTML + "\n"  +"- " +precio.innerHTML + "\n");
     } else {
         let curso = document.getElementById("info-card-title6");
         let detalle = document.getElementById("info-detalle6");
         let precio = document.getElementById("info-precio6");
-    alert("CURSO: " + "\n" + curso.innerHTML + "\n" + "Detalle: " + "\n" + detalle.innerHTML + "\n" +"Precio: " + "\n" + precio.innerHTML + "\n");
+    alert("INFO DEL CURSO: " + "\n" +"- "+ curso.innerHTML  + "\n" + "- " + detalle.innerHTML + "\n" +"- " + precio.innerHTML + "\n");
     }
     
 
